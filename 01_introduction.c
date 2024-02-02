998ed99352b8270b300e45db391a0c79fc602c71
Muhammad-Usama123 <osamaa.g777@gmail.com>
2024-02-02 19:41:42 +0500

    1st commit after writing reserve keywords in c fri 02/02/2024 19:41

---
+++ 01_introduction.c
@@ -0,0 +1,7 @@
+// Uses Of C Programming Language:
+
+    // used in making operating systems like linux, windows etc.
+
+    // used for making device driver softwares.
+
+    // used for making programs for devices that uses microchip like microwave, functions of cars etc. etc. cause c language requires low memory and it executes faster.
---
+++ first.c
@@ -0,0 +1,87 @@
+# include<stdio.h>
+
+int main () {
+    printf("I am learning C.");
+    return 0;
+}
+
+// Variables: 
+
+    // variable is a container which stores a value and this value gets stored in the memory of computer. Variables in C language stores value of a constant. Variables names can be changed. Example:
+
+        // a = 3;
+        // b = 4.7;
+        // c = 'A' 
+
+        // Rules for naming variables in C:
+
+            // 1st character must be an alphabet or underscore(_).
+
+            // No commas, blanks allowed.
+
+            // No special symbol other than underscore(_) allowed.
+
+            // Variables names are case sensitive.
+
+            // Note: we should create meaningful variable names in our programs for readability purposes.
+
+    // Constants: 
+
+        // constant means the value cannot be changed.
+
+        // Types Of Constants:
+
+            // Primarily there are 3 types of constants.
+
+                // 1) Integer constant. examples: -1, 6, 7, 9
+
+                // 2) Real constant. examples: -322.1, 2.5, 7.0
+
+                // 3) Character constant. examples: 'a', '$', '@' (Must be enclosed within single inverted commas)
+
+    // Keywords:
+
+        // reserved words. 32 keywords according to Haris notes in C below:
+
+            /*
+            auto
+            break;
+            case
+            char
+            const
+            continue;
+            default
+            do
+            double
+            long
+            return
+            register
+            short
+            signed
+            sizeof
+            static
+            int
+            else
+            enum
+            extern
+            float
+            for 
+            goto
+            if
+            struct
+            switch
+            typedef
+            union
+            unsigned
+            void
+            volatile
+            while
+            */
+            
+            
+            
+            
+
+
+
+    
