be40a9e0903dc738755d89e90f19b9c63782d5c7
Muhammad-Usama123 <osamaa.g777@gmail.com>
2024-02-03 06:04:17 +0500

    2nd commit saturday start 03/02/2024 06:04

--- first.c
+++ first.c
@@ -1,9 +1,9 @@
-# include<stdio.h>
+# include<stdio.h> // pre processor directive.
 
-int main () {
-    printf("I am learning C.");
+int main () { 
+    printf("I am learning C."); // printf is also a function. So this is a example of function inside function.
     return 0;
-}
+} //  this int main  is  a function.
 
 // Variables: 
 
@@ -25,59 +25,86 @@ int main () {
 
             // Note: we should create meaningful variable names in our programs for readability purposes.
 
-    // Constants: 
+// Constants: 
 
-        // constant means the value cannot be changed.
+    // constant means the value cannot be changed.
 
-        // Types Of Constants:
+    // Types Of Constants:
 
-            // Primarily there are 3 types of constants.
+        // Primarily there are 3 types of constants.
 
-                // 1) Integer constant. examples: -1, 6, 7, 9
+            // 1) Integer constant. examples: -1, 6, 7, 9
 
-                // 2) Real constant. examples: -322.1, 2.5, 7.0
+            // 2) Real constant. examples: -322.1, 2.5, 7.0
 
-                // 3) Character constant. examples: 'a', '$', '@' (Must be enclosed within single inverted commas)
+            // 3) Character constant. examples: 'a', '$', '@' (Must be enclosed within single inverted commas)
 
-    // Keywords:
+// Keywords:
 
-        // reserved words. 32 keywords according to Haris notes in C below:
+    // reserved words. 32 keywords according to Haris notes in C below:
 
-            /*
-            auto
-            break;
-            case
-            char
-            const
-            continue;
-            default
-            do
-            double
-            long
-            return
-            register
-            short
-            signed
-            sizeof
-            static
-            int
-            else
-            enum
-            extern
-            float
-            for 
-            goto
-            if
-            struct
-            switch
-            typedef
-            union
-            unsigned
-            void
-            volatile
-            while
-            */
+        /*
+        auto
+        break;
+        case
+        char
+        const
+        continue;
+        default
+        do
+        double
+        long
+        return
+        register
+        short
+        signed
+        sizeof
+        static
+        int
+        else
+        enum
+        extern
+        float
+        for 
+        goto
+        if
+        struct
+        switch
+        typedef
+        union
+        unsigned
+        void
+        volatile
+        while
+        */
+
+// Basic Structure Of A C Program: (Rules of writing c program)
+
+    // Every C program's execution starts from main() function and executes instructions present inside this main function. 
+
+    // Each instruction is terminated with a semicolon(;).
+
+    // Instructions are case-sensitive.
+
+    // Instructions are executed in the same order in which they are written.
+
+// Comments: 
+
+    // 1) //
+
+    // 2) /* */
+
+// Compilation And Execution Of A C Program:
+
+    // first.c (file in vscode etc.) ==> C Compiler (gcc compiler etc.) ==> first.exe
+
+// gcc first.c // This command will maybe return the .exe file with the name of a.exe.
+// gcc first.c -o first.exe // command for getting the exe file with our given exe file name in this command after -o flag.
+
+// ./first.exe // command for running the above first.exe file that our compiler will return to us after running the command in line 102.
             
+
+    
             
             
             
