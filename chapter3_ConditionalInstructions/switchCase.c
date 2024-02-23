f6e84387d8d1c2a35daa828bba47f26b73aa4801
Muhammad-Usama123 <osamaa.g777@gmail.com>
2024-02-23 08:46:07 +0500

    chapter 3 ps

---
+++ chapter3_ConditionalInstructions/switchCase.c
@@ -0,0 +1,21 @@
+// Switch Case Control Instruction:
+
+    // #include <stdio.h>
+    
+    // int main(){
+    //     int a;
+    //     switch (a) // char values and integer-expressions can be written in this bracket
+    // {
+    // case1:
+    // printf("abcd"); 
+    //     break; // for breaking this switch case control instruction when any of the case is matched. 
+    
+    // default: // If none of the case matches then this default statement will run
+    // printf("defg");
+    //     break;
+    // }
+    //     return 0;
+    // }
+
+// (int) floatValueVariable // for converting float value into integer.
+    
