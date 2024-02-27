5515cd1c9899e1e463b48bf0a5452e571e49dc46
Muhammad-Usama123 <osamaa.g777@gmail.com>
2024-02-27 13:00:33 +0500

    Arrays complete practice set remaining

--- chapter7_Arrays/arrays.c
+++ chapter7_Arrays/arrays.c
@@ -87,3 +87,117 @@
                     // Subtraction of 1 pointer from another 
                     // Comparision of 2 pointer variables.
 
+    // Accessing Arrays Using Pointers:
+
+        // Consider This Example:
+
+            // #include <stdio.h>
+                
+            // int main(){
+            //     int marks[4];
+            //     int *ptr;
+                //     ptr = &marks[0]; // This way we can have an integer pointer pointing to first element of the array like this. // ptr = marks and ptr = &marks[0] will do the same thing means it will point to element at 0th index in marks array.
+
+            //     for (int i = 0; i < 4; i++)
+            //     {
+            //         printf("Enter The Marks of student %d \n", i + 1);
+            //         scanf("%d", ptr);
+            //         ptr ++;
+            //     }
+
+            //     for (int i = 0; i < 4; i++)
+            //     {
+                //         printf("The marks of student %d is %d \n", i + 1, marks[i]);
+            //     }
+                    
+                    
+            //     return 0;
+            // }
+
+                // In above example if ptr points to index 0, ptr ++ will point to index 1 and so on.
+
+    // Passing Arrays To Functions:
+
+        // Arrays can be passed to the functions like this:
+
+            // #include <stdio.h>
+
+            // void printArray(int *ptr, int n){
+            //     for (int i = 0; i < n; i++)
+            //     {
+            //         printf("The value of element %d is %d\n", i + 1, *(ptr+i));
+            //     } // function can be defined with prototype but good practice is define function separately from function prototype.
+
+                                                // OR 
+
+            // void printArray(int ptr[], int n){
+            //     for (int i = 0; i < n; i++)
+            //     {
+            //         printf("The value of element %d is %d\n", i + 1, ptr[i]);
+            //     } 
+
+                // ptr[2] = 5555 // This value will also change in the original arr array in main function which we passed to this user defined function as an argument.
+                
+            // }
+            
+            // int main(){
+            //     int arr[] = {11, 22, 33, 44, 55, 66, 77};
+            //     printArray(arr, 7);
+            //     return 0;
+            // }
+
+            // In array we can change the value of an array from a userdefined funtion by just passing our array to that user defined funtion and change its value inside that user defined function it will also change the value in original array cause arrays pass reference not copy of value.
+
+    // MultiDimensional Arrays:
+
+        // An array can be of 2 dimension/ 3 dimension/ n dimensions
+
+            // A 2 dimensional array can be defined as:
+
+                // int arr [3][2] = {
+                //     {1 , 4}
+                //     {7 , 9}
+                //     {11, 22}
+                // };
+
+                    // We can access the elements of this array as 
+                        // arr[0][0] // on this index value of element is 1 
+                        // arr [0][1] // on this index value of element is 4
+
+            // Example:
+
+                // #include <stdio.h>
+                
+                // int main(){
+                //     int n_students = 3;
+                //     int n_subjects = 5;
+                //     int marks[3][5];
+
+                //     for (int i = 0; i < n_students; i++)
+                //     {
+                //         for (int j = 0; j < n_subjects; j++)
+                //         {
+                //             printf("Enter the marks of student %d in subject %d \n", i + 1, j + 1);
+                //             scanf("%d", &marks[i][j]);11
+                //         }
+                        
+                //     }
+
+                //     for (int i = 0; i < n_students; i++)
+                //     {
+                //         for (int j = 0; j < n_subjects; j++)
+                //         {
+                //             printf("The marks of student %d in subject %d are %d \n", i + 1, j + 1, marks[i][j]);
+                //         }
+                        
+                //     }
+                    
+                //     return 0;
+                // }
+
+                // 2-D Arrays In Memory:
+
+                    // A 2-D array is also get stored in contiguous memory blocks like a 1-D array like this:
+
+                        // [1]     [4]     [7]      [9]      [11]      [22] //contiguous blocks joint.
+                        // 87224   87228.......... // In above line 202 spaces are because I wanted to write their addresses below them actually they are joint contigous memory blocks means there is no space between these blocks. // square brackets in line 202 are for showing contigous memory blocks of a 2 dimensional array. 
